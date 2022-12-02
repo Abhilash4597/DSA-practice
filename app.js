3e894aae718b0af6266d2685da4253d912e06a7b
Abhilash4597 <abhilash4597@gmail.com>
2022-12-02 20:29:11 +0530

    leetcode question 283

--- app.js
+++ app.js
@@ -241,3 +241,20 @@ var isPalindrome = function(s) {
     }
 };
 console.log(isPalindrome("A man, a plan, a canal: Panama"));
+
+// ============================================================question-283 =================================================================================
+
+var moveZeroes = function(nums) {
+    let zero=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==0){
+            nums[zero]=nums[i];
+            zero++;
+        }
+    }
+    for(let j=zero;j<nums.length;j++){
+        nums[j]=0;
+    }
+    return nums;
+};
+console.log(moveZeroes([0,1,0,3,12]));
